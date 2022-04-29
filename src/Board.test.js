809a4fc3e2b2ee4066577139695e12f41ab89e10
mdwiltfong <wiltfong.michael1@gmail.com>
2022-04-28 21:14:22 -0400

    initial mock test

--- src/Board.test.js
+++ src/Board.test.js
@@ -1,3 +1,15 @@
 import React from "react";
 import Board from "./Board";
 import { render } from "@testing-library/react";
+const [mockBoard, setMockBoard] = React.useState();
+
+jest.mock("./Board.js", () => {
+  const initialBoardState = [
+    [true, false, true, true],
+    [true, true, false, true],
+    [true, true, false, true],
+    [false, false, false, true],
+  ];
+  React.useState = jest.fn().mockReturnValue(initialBoardState);
+  render(<Board />);
+});
